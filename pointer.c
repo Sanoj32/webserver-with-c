2a1d333b341cdb4ebab6c5db29ad36d4a05ea726
Sanoj Raj Shrestha <sanoj.shrestha.13@gmail.com>
2025-10-03 17:14:31 +0545

    pointer stuff

---
+++ pointer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int var = 20;   /* actual variable declaration */
+    int *ip;        /* pointer variable declaration */
+    ip = &var;  /* store address of var in pointer variable*/
+    printf("Address of var variable: %p\n", &var  );
+    return 0;
+}
